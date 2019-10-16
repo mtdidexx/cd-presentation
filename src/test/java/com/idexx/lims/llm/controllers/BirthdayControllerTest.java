b090e50a2d69509bb7939e1fcbdd3b1f0da62a60
Mark Donahue <mark-donahue@idexx.com>
2019-10-16 08:46:05 -0400

    Add GoCD integration pipeline

--- src/test/java/com/idexx/lims/llm/controllers/BirthdayControllerTest.java
+++ src/test/java/com/idexx/lims/llm/controllers/BirthdayControllerTest.java
@@ -1,10 +1,28 @@
 package com.idexx.lims.llm.controllers;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BirthdayControllerTest {
+
+    private BirthdayController controller;
+
     @Before
     public void setup() {
-        BirthdayController controller = new BirthdayController(null);
+        controller = new BirthdayController(null);
+    }
+
+    @After
+    public void tearDown() {
+        controller = null;
+    }
+
+    @Test
+    public void birthday() {
+        String result = controller.getDayOfWeek("2019-10-15");
+        assertEquals("foo", result);
     }
 }
